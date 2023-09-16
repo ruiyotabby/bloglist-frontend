f611dfc48dddd5fd4d829a96ae1939c50ad59d91
Ruiyot Abby <ruiyotabby@gmail.com>
2023-09-16 13:16:18 +0300

    Test if blogs are listed according to likes

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,12 +1,20 @@
 describe('Blog app', () => {
   beforeEach(function () {
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
-    const user = {
+    const user1 = {
       name: 'john',
       username: 'johny',
       password: 'john'
     }
-    cy.request('POST',  `${Cypress.env('BACKEND')}/users`, user)
+
+    const user2 = {
+      name: 'mike',
+      username: 'michael',
+      password: 'mike'
+    }
+
+    cy.request('POST',  `${Cypress.env('BACKEND')}/users`, user1)
+    cy.request('POST',  `${Cypress.env('BACKEND')}/users`, user2)
     cy.visit('')
   })
 
@@ -51,6 +59,7 @@ describe('Blog app', () => {
       cy.get('[name=url]').type('a url by cypress')
       cy.contains('create').click()
 
+      cy.get('#root').should('have.descendants', '.blog')
       cy.get('.success').contains("'a title by cypress' by 'an author by cypress' added")
       cy.contains('a title by cypress an author by cypress')
     })
@@ -69,6 +78,61 @@ describe('Blog app', () => {
         cy.contains('like').click()
 
         cy.get('#likes').contains(1)
+      })
+
+      it('A blog can be deleted by the user who created it', function () {
+        cy.contains('show').click()
+        cy.contains('remove').click()
+        cy.get('.success').contains("blog 'a title by cypress an author by cypress' was deleted")
+        cy.get('#root').should('not.have.descendants', '.blog')
+      })
+
+      it('A blog cannot be deleted by the user who didn\'t created it', function () {
+        cy.contains('log out').click()
+        cy.wait(300)
+        cy.login({ username: 'michael', password: 'mike' })
+        cy.contains('show').click()
+        cy.get('.blog').should('not.contain', 'remove')
+      })
+    })
+
+    describe('when many blogs are created', function () {
+      beforeEach(function () {
+        cy.createBlog({
+          title: 'the title with the least likes',
+          author: 'an author by cypress',
+          url: 'a url by cypress'
+        })
+
+        cy.createBlog({
+          title: 'the title with the second most likes',
+          author: 'an author by cypress',
+          url: 'a url by cypress'
+        })
+
+        cy.createBlog({
+          title: 'the title with the most likes',
+          author: 'an author by cypress',
+          url: 'a url by cypress'
+        })
+      })
+
+      it('Blogs should be listed according to likes', function () {
+        cy.contains('the title with the most likes').parent().as('first')
+        cy.get('@first').contains('show').click()
+
+        for (let i = 0; i < 3; i++) {
+          cy.get('@first').contains( 'button', 'like').click()
+          cy.wait(800)
+        }
+
+        cy.contains('the title with the second most likes').parent().as('second')
+        cy.get('@second').contains('show').click()
+        cy.get('@second').contains( 'button', 'like').click()
+
+        cy.get('.blog').eq(0).should('contain', 'the title with the most likes')
+        cy.get('.blog').eq(1).should('contain', 'the title with the second most likes')
+        cy.get('.blog').eq(2).should('contain', 'the title with the least likes')
       })
     })
   })
