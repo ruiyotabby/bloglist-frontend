14899ccc9bad9d67e24a8005219472e8272fa424
Ruiyot Abby <ruiyotabby@gmail.com>
2023-11-12 01:22:11 +0300

    Update User view

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,64 +1,66 @@
-import { useQuery } from "@tanstack/react-query"
-import userService from "../services/user"
-import { Link, useParams } from "react-router-dom"
+import { useQuery } from '@tanstack/react-query';
+import userService from '../services/user';
+import { Link, useParams } from 'react-router-dom';
 
 const Users = () => {
-  const { error, isLoading, data } = useQuery({
-    queryKey: ['users'],
-    queryFn: userService.getAll
-  })
-  if (isLoading) return <p>Please wait...</p>
-  if (error) return <p>{error.message}</p>
-  const users = data
+	const { error, isLoading, data } = useQuery({
+		queryKey: ['users'],
+		queryFn: userService.getAll,
+	});
+	if (isLoading) return <p>Please wait...</p>;
+	if (error) return <p>{error.message}</p>;
+	const users = data;
 
-  return (
-    <>
-      <h3>Users</h3>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>blogs created</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user =>
-          <tr key={user.id}>
-            <td><Link to={`/users/${user.id}`}>{user.name}</Link></td>
-            <td>{user.blogs.length}</td>
-          </tr>
-          )}
-        </tbody>
-      </table>
-    </>
-  )
-}
+	return (
+		<>
+			<h3>Users</h3>
+			<table>
+				<thead>
+					<tr>
+						<th></th>
+						<th>blogs created</th>
+					</tr>
+				</thead>
+				<tbody>
+					{users.map((user) => (
+						<tr key={user.id}>
+							<td>
+								<Link to={`/users/${user.id}`}>{user.name}</Link>
+							</td>
+							<td>{user.blogs.length}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</>
+	);
+};
 
 export const User = () => {
-  const { id } = useParams()
-  const { isLoading, data } = useQuery({
-    queryKey: ['users'],
-    queryFn: userService.getAll
-  })
+	const { id } = useParams();
+	const { isLoading, data } = useQuery({
+		queryKey: ['users'],
+		queryFn: userService.getAll,
+	});
 
-  if (isLoading) return <p>Please wait...</p>
+	if (isLoading) return <p>Please wait...</p>;
 
-  const users = data
-  const user = users.find(user => user.id === id)
+	const users = data;
+	const user = users.find((user) => user.id === id);
 
-  if (!user) return <p>User not found</p>
+	if (!user) return <p>User not found</p>;
 
-  return (
-    <>
-      <h3>{user.name}</h3>
-      <h5>added blogs</h5>
-      <ul>
-        {user.blogs.map(blog =>
-          <li key={blog.id}><Link to={`/blogs/${blog.id}`}>{blog.title}</Link></li>
-        )}
-      </ul>
-    </>
-  )
-}
+	return (
+		<>
+			<h3>{user.name}</h3>
+			<h5>added blogs</h5>
+			{user.blogs.map((blog) => (
+				<Link key={blog.id} className="blog" to={`/blogs/${blog.id}`}>
+					{blog.title}{' '}
+				</Link>
+			))}
+		</>
+	);
+};
 
-export default Users
+export default Users;
